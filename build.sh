f158c85dab7ee2ca24a70ad3279629557202dc92
Victor Augusto <victorcosta939@gmail.com>
2025-06-08 15:31:47 -0300

    update dos arquivos de build e teste

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@
 bison -d parser.y -o parser.tab.c || { echo "Bison failed"; exit 1; }
 flex lexer.l || { echo "Flex failed"; exit 1; }
 gcc parser.tab.c lex.yy.c -Wall -g -O0 -o cimples_compiler|| { echo "Compilation failed"; exit 1; }
-
-./cimples_compiler
